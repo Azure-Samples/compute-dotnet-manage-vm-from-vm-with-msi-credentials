8bdb805aa9e5d67b49d7c17480867ee3f650c40b
Chengming <v-cruan@microsoft.com>
2023-10-25 15:10:38 +0800

    update

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ using Azure.ResourceManager.Compute.Models;
 using Azure.ResourceManager.Compute;
 using System.Net.NetworkInformation;
 using System.Xml.Linq;
+using Azure.Core.Pipeline;
 
 namespace ManageVirtualMachineFromMSIEnabledVirtualMachine
 {
@@ -32,54 +33,129 @@ namespace ManageVirtualMachineFromMSIEnabledVirtualMachine
             // see https://github.com/Azure-Samples/compute-dotnet-manage-user-assigned-msi-enabled-virtual-machine.git
             //
 
-            var clientId = Environment.GetEnvironmentVariable("CLIENT_ID");
-            var clientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET");
-            var tenantId = Environment.GetEnvironmentVariable("TENANT_ID");
-            var subscriptionId = Environment.GetEnvironmentVariable("SUBSCRIPTION_ID");
-            ClientSecretCredential credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
-            ArmClient client = new ArmClient(credential, subscriptionId);
+            string usage = "Usage: dotnet run <subscription-id> <rg-name> [<client-id>]";
+            if (args.Length < 2)
+            {
+                throw new ArgumentException(usage);
+            }
 
-            string resourceGroupName = Utilities.CreateRandomName("ComputeSampleRG");
+            string subscriptionId = args[0];
+            string resourceGroupName = args[1];
+            string clientId = args.Length > 2 ? args[2] : null;
             string linuxVMName = Utilities.CreateRandomName("vm");
             string userName = Utilities.CreateUsername();
             string password = Utilities.CreatePassword();
-            AzureLocation region = AzureLocation.EastUS;
+
+
 
             //=============================================================
             // MSI Authenticate
 
-            AzureCredentials msiCredentails = new AzureCredentials(new MSILoginInformation(MSIResourceType.VirtualMachine)
+            var credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions
             {
-                UserAssignedIdentityClientId = clientId
-            }, AzureEnvironment.AzureGlobalCloud);
+                ManagedIdentityClientId = clientId
+            });
 
-            var azure = Azure
-                .Configure()
-                .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
-                .Authenticate(msiCredentails)
-                .WithDefaultSubscription();
+            ArmClient client = new ArmClient(credential, subscriptionId);
+            SubscriptionResource subscription = client.GetDefaultSubscription();
+            var resourceGroupLro = subscription.GetResourceGroups().Get(resourceGroupName);
+            ResourceGroupResource resourceGroup = resourceGroupLro.Value;
 
-            Console.WriteLine("Selected subscription: " + azure.SubscriptionId);
+            Console.WriteLine("Selected subscription: " + subscription.Data.Id);
 
             //=============================================================
             // Create a Linux VM using MSI credentials
 
             Console.WriteLine("Creating a Linux VM using MSI credentials");
 
-            var virtualMachine = azure.VirtualMachines
-                    .Define(linuxVMName)
-                    .WithRegion(region)
-                    .WithExistingResourceGroup(resourceGroupName)
-                    .WithNewPrimaryNetwork("10.0.0.0/28")
-                    .WithPrimaryPrivateIPAddressDynamic()
-                    .WithoutPrimaryPublicIPAddress()
-                    .WithPopularLinuxImage(KnownLinuxVirtualMachineImage.UbuntuServer16_04_Lts)
-                    .WithRootUsername(userName)
-                    .WithRootPassword(password)
-                    .WithSize(VirtualMachineSizeTypes.Parse("Standard_D2a_v4"))
-                    .Create();
+            Utilities.Log("Pre-creating some resources that the VM depends on");
 
-            Console.WriteLine($"Created virtual machine using MSI credentials: {virtualMachine.Id}");
+            // Creating a virtual network
+            Utilities.Log("Creating virtual network...");
+            string vnetName = Utilities.CreateRandomName("vnet");
+            VirtualNetworkData vnetInput = new VirtualNetworkData()
+            {
+                Location = resourceGroup.Data.Location,
+                AddressPrefixes = { "10.10.0.0/16" },
+                Subnets =
+                    {
+                        new SubnetData() { Name = "subnet1", AddressPrefix = "10.10.1.0/24"},
+                        new SubnetData() { Name = "subnet2", AddressPrefix = "10.10.2.0/24"},
+                    },
+            };
+            var vnetLro = resourceGroup.GetVirtualNetworks().CreateOrUpdate(WaitUntil.Completed, vnetName, vnetInput);
+            Utilities.Log($"Created a virtual network: {vnetLro.Value.Data.Name}");
+
+            // Creating network interface
+            Utilities.Log($"Creating network interface...");
+            string nicName = Utilities.CreateRandomName("nic");
+            var nicInput = new NetworkInterfaceData()
+            {
+                Location = resourceGroup.Data.Location,
+                IPConfigurations =
+                    {
+                        new NetworkInterfaceIPConfigurationData()
+                        {
+                            Name = "default-config",
+                            PrivateIPAllocationMethod = NetworkIPAllocationMethod.Dynamic,
+                            Subnet = new SubnetData()
+                            {
+                                Id = vnetLro.Value.Data.Subnets[0].Id
+                            },
+                        }
+                    }
+            };
+            var networkInterfaceLro = resourceGroup.GetNetworkInterfaces().CreateOrUpdate(WaitUntil.Completed, nicName, nicInput);
+            Utilities.Log($"Created network interface: {networkInterfaceLro.Value.Data.Name}");
+
+            Utilities.Log("Creating a Linux VM with MSI associated and install DotNet and Git");
+
+            VirtualMachineData linuxVMInput = new VirtualMachineData(resourceGroup.Data.Location)
+            {
+                HardwareProfile = new VirtualMachineHardwareProfile()
+                {
+                    VmSize = VirtualMachineSizeType.StandardF2
+                },
+                StorageProfile = new VirtualMachineStorageProfile()
+                {
+                    ImageReference = new ImageReference()
+                    {
+                        Publisher = "Canonical",
+                        Offer = "UbuntuServer",
+                        Sku = "16.04-LTS",
+                        Version = "latest",
+                    },
+                    OSDisk = new VirtualMachineOSDisk(DiskCreateOptionType.FromImage)
+                    {
+                        OSType = SupportedOperatingSystemType.Linux,
+                        Caching = CachingType.ReadWrite,
+                        ManagedDisk = new VirtualMachineManagedDisk()
+                        {
+                            StorageAccountType = StorageAccountType.StandardLrs
+                        }
+                    },
+                },
+                OSProfile = new VirtualMachineOSProfile()
+                {
+                    AdminUsername = userName,
+                    AdminPassword = password,
+                    ComputerName = linuxVMName,
+                },
+                NetworkProfile = new VirtualMachineNetworkProfile()
+                {
+                    NetworkInterfaces =
+                        {
+                            new VirtualMachineNetworkInterfaceReference()
+                            {
+                                Id = networkInterfaceLro.Value.Data.Id,
+                                Primary = true,
+                            }
+                        }
+                },
+            };
+            var linuxVmLro = resourceGroup.GetVirtualMachines().CreateOrUpdate(WaitUntil.Completed, linuxVMName, linuxVMInput);
+
+            Console.WriteLine($"Created virtual machine {linuxVmLro.Value.Data.Name} using MSI credentials: ");
         }
     }
 }
